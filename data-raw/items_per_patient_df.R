3a4cec022096255630de3642ddb9418942da63bc
philgodfrey <philip.godfrey@nhs.net>
2021-10-14 12:55:11 +0100

    Styled `items_per_patient_df.R`

--- data-raw/items_per_patient_df.R
+++ data-raw/items_per_patient_df.R
@@ -11,16 +11,16 @@ item_level_df <- dplyr::tbl(
   from = dbplyr::sql("SELECT * FROM DALL_REF.INT615_ITEM_LEVEL_BASE")
 )
 
-# Aggregate data to return: 
+# Aggregate data to return:
 # Monthly average number of items per patient
 # By carehome flag (CH_FLAG)
-items_per_patient_df <- item_level_df %>% 
-  dplyr::group_by(YEAR_MONTH, CH_FLAG) %>% 
-    dplyr::summarise(
-      TOTAL_ITEMS = dplyr::sum(ITEM_COUNT),
-      TOTAL_PATIENTS = dplyr::n_distinct(NHS_NO),
-      ITEMS_PER_PATIENT = dplyr::sum(ITEM_COUNT)/dplyr::n_distinct(NHS_NO)
-  ) %>% 
+items_per_patient_df <- item_level_df %>%
+  dplyr::group_by(YEAR_MONTH, CH_FLAG) %>%
+  dplyr::summarise(
+    TOTAL_ITEMS = dplyr::sum(ITEM_COUNT),
+    TOTAL_PATIENTS = dplyr::n_distinct(NHS_NO),
+    ITEMS_PER_PATIENT = dplyr::sum(ITEM_COUNT) / dplyr::n_distinct(NHS_NO)
+  ) %>%
   dplyr::collect()
 
 # Add to data-raw/
