838de5adc33e2ab48a56bca8945733671d148e88
adamivisonnhs <adam.ivison@nhs.net>
2021-11-02 10:17:15 +0000

    Count by YEAR_MONTH and overall

--- data-raw/patients_by_gender_and_age_band_df.R
+++ data-raw/patients_by_gender_and_age_band_df.R
@@ -10,8 +10,8 @@ fact_db <- dplyr::tbl(
   from = dbplyr::sql("SELECT * FROM DALL_REF.INT615_ITEM_LEVEL_BASE")
 )
 
-# Number of patients of each gender by care home flag
-patients_by_gender_and_age_band_df <- fact_db %>%
+# Add gender and age band groups to the FACT table
+fact_db <- fact_db %>%
   dplyr::mutate(
     PDS_GENDER = dplyr::case_when(
       PDS_GENDER == 1 ~ "Male",
@@ -27,22 +27,39 @@ patients_by_gender_and_age_band_df <- fact_db %>%
       CALC_AGE < 90 ~ "85-89",
       TRUE ~ "90+"
     )
-  ) %>%
-  dplyr::group_by(PDS_GENDER, AGE_BAND) %>%
-  dplyr::summarise(
-    TOTAL_PATIENTS = dplyr::n_distinct(NHS_NO), 
-    .groups = "drop_last"
-  ) %>% 
-  dplyr::mutate(PCT = TOTAL_PATIENTS / sum(TOTAL_PATIENTS)) %>% 
-  dplyr::ungroup() %>%
-  dplyr::collect() %>%
-  # Format columns for highcharter
-  dplyr::mutate(
-    PDS_GENDER = forcats::fct_relevel(PDS_GENDER, "Male", "Female", "Unknown")
+  )
+
+# Total patients by gender and age band
+patients_by_gender_and_age_band_df <-
+  rbind(
+
+    # Overall
+    fact_db %>%
+      dplyr::mutate(YEAR_MONTH = "Overall") %>%
+      dplyr::group_by(YEAR_MONTH, PDS_GENDER, AGE_BAND) %>%
+      dplyr::summarise(
+        TOTAL_PATIENTS = dplyr::n_distinct(NHS_NO),
+        .groups = "drop_last"
+      ) %>%
+      dplyr::mutate(PCT = TOTAL_PATIENTS / sum(TOTAL_PATIENTS)) %>%
+      dplyr::ungroup() %>%
+      dplyr::collect(),
+
+    # By year month
+    fact_db %>%
+      dplyr::group_by(YEAR_MONTH, PDS_GENDER, AGE_BAND) %>%
+      dplyr::summarise(
+        TOTAL_PATIENTS = dplyr::n_distinct(NHS_NO),
+        .groups = "drop_last"
+      ) %>%
+      dplyr::mutate(PCT = TOTAL_PATIENTS / sum(TOTAL_PATIENTS)) %>%
+      dplyr::ungroup() %>%
+      dplyr::arrange(YEAR_MONTH) %>%
+      dplyr::collect()
   )
 
 # Add to data-raw/
-usethis::use_data(patients_by_gender_df, overwrite = TRUE)
+usethis::use_data(patients_by_gender_and_age_band, overwrite = TRUE)
 
 # Disconnect from database
 DBI::dbDisconnect(con)
