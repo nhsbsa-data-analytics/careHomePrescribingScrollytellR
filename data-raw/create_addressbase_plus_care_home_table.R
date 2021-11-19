bb0b02f3e4499add08ff672921b79b8ce373f937
AdnanS <adnan.shroufi@nhs.net>
2021-11-19 13:00:56 +0000

    edit and streamline of 2 ab_plus code tables

--- data-raw/create_addressbase_plus_care_home_table.R
+++ data-raw/create_addressbase_plus_care_home_table.R
@@ -1,4 +1,5 @@
-library(magrittr)
+library(dplyr)
+library(dbplyr)
 
 # Set up connection to the DB
 con <- nhsbsaR::con_nhsbsa(database = "DALP")
@@ -9,13 +10,13 @@ exists <- DBI::dbExistsTable(conn = con, name = "ADDRESSBASE_PLUS_CARE_HOME")
 # Drop any existing table beforehand
 if (exists) {
   DBI::dbRemoveTable(conn = con, name = "ADDRESSBASE_PLUS_CARE_HOME")
-}
+  }
+
+# Part One: AddressBase Plus base table ----------------------------------------
 
 # Create a lazy table from the AddressBase Plus table
-addressbase_plus_db <- dplyr::tbl(
-  src = con,
-  from = dbplyr::sql("SELECT * FROM DALL_REF.ADDRESSBASE_PLUS")
-)
+addressbase_plus_db <- con %>% 
+  tbl(from = in_schema("DALL_REF", "ADDRESSBASE_PLUS"))
 
 # Filter AddressBase Plus to English properties in at the end of 2021 FY and
 # create a care home flag
@@ -42,236 +43,87 @@ care_home_postcodes_db <- addressbase_plus_db %>%
 addressbase_plus_db <- addressbase_plus_db %>%
   dplyr::inner_join(y = care_home_postcodes_db)
 
-# Create a long table of single line addresses
+# Create a Wide table of 3 single line address types
 addressbase_plus_db <- addressbase_plus_db %>%
   addressMatchR::calc_addressbase_plus_dpa_single_line_address() %>%
   addressMatchR::calc_addressbase_plus_geo_single_line_address() %>%
   dplyr::select(
     UPRN,
+    CLASS,
     CH_FLAG,
-    DPA_POSTCODE = POSTCODE,
+    AB_POSTCODE = POSTCODE_LOCATOR,
     DPA_SINGLE_LINE_ADDRESS,
-    GEO_POSTCODE = POSTCODE_LOCATOR,
     GEO_SINGLE_LINE_ADDRESS
-  ) %>%
+  ) %>% 
+  addressMatchR::tidy_postcode(col = AB_POSTCODE) %>% 
+  addressMatchR::tidy_single_line_address(col = DPA_SINGLE_LINE_ADDRESS) %>% 
+  addressMatchR::tidy_single_line_address(col = GEO_SINGLE_LINE_ADDRESS) %>% 
+  merge_address_strings(
+    col_one = DPA_SINGLE_LINE_ADDRESS,
+    col_two = GEO_SINGLE_LINE_ADDRESS
+    ) %>% 
+  rename(CORE_SINGLE_LINE_ADDRESS = MERGE_STRING)
+
+# Convert to a long table of distinct stacked single line addresses
+addressbase_plus_db <- addressbase_plus_db %>%
   tidyr::pivot_longer(
-    cols = -c(UPRN, CH_FLAG),
-    names_to = c("ADDRESS_TYPE", ".value"),
-    names_sep = "_" # Should use names_pattern but can't get it to work
-  ) %>%
-  # Hack the names_sep result back to what it should be (ignore warning as
-  # splits {DPA,GEO}_SINGLE_LINE_ADDRESS into {DPA,GEO} / SINGLE / LINE /
-  # ADDRESS and drops LINE / ADDRESS as no data exists)
-  dplyr::rename(SINGLE_LINE_ADDRESS = SINGLE) %>%
-  # Remove NA POSTCODE / SINGLE_LINE_ADDRESS (these are DPA)
-  dplyr::filter(!is.na(POSTCODE) & !is.na(SINGLE_LINE_ADDRESS))
-
-# Tidy postcode and format single line addresses for tokenisation
-addressbase_plus_db <- addressbase_plus_db %>%
-  addressMatchR::tidy_postcode(col = POSTCODE) %>%
-  addressMatchR::tidy_single_line_address(col = SINGLE_LINE_ADDRESS)
-
-# Combine rows where there DPA and GEO single line addresses are equal into BOTH
-addressbase_plus_db <- addressbase_plus_db %>%
-  dplyr::mutate(
-    DPA = ifelse(ADDRESS_TYPE == "DPA", 1, 0),
-    GEO = ifelse(ADDRESS_TYPE == "GEO", 1, 0)
-  ) %>%
-  dplyr::group_by(dplyr::across(c(-ADDRESS_TYPE, -DPA, -GEO))) %>%
-  dplyr::summarise(DPA = sum(DPA), GEO = sum(GEO), .groups = "drop") %>%
-  dplyr::mutate(
-    ADDRESS_TYPE = dplyr::case_when(
-      DPA == 1 & GEO == 1 ~ "BOTH",
-      DPA == 1 & GEO == 0 ~ "DPA",
-      DPA == 0 & GEO == 1 ~ "GEO",
-      TRUE ~ "ERROR"
-    )
-  ) %>%
-  dplyr::select(-DPA, -GEO) %>%
-  dplyr::relocate(ADDRESS_TYPE, .after = CH_FLAG)
-
-# Pull the UPRNs that have two different addresses
-combined_addresses_db <- addressbase_plus_db %>%
-  dplyr::group_by(UPRN) %>%
-  dplyr::summarise(NO_ADDRESS_TYPE = dplyr::n_distinct(ADDRESS_TYPE)) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(NO_ADDRESS_TYPE > 1) %>%
-  dplyr::select(UPRN) %>%
-  dplyr::inner_join(y = addressbase_plus_db)
-
-# Create a combined single line address for these of the form "{DPA} / {GEO}"
-combined_addresses_db <- combined_addresses_db %>%
-  tidyr::pivot_wider(
-    id_cols = c(UPRN, CH_FLAG, POSTCODE),
-    names_from = ADDRESS_TYPE,
-    values_from = SINGLE_LINE_ADDRESS
-  ) %>%
-  dplyr::mutate(SINGLE_LINE_ADDRESS = paste0(DPA, " / ", GEO)) %>%
-  dplyr::select(-DPA, -GEO)
-
-# Compute query and save in temporary remote table
-combined_addresses_db %>%
-  nhsbsaR::oracle_create_table(table_name = "TMP_COMBINED_ADDRESSES")
-
-# Pull the temporary remote table
-combined_addresses_db <- dplyr::tbl(
-  src = con,
-  from = dbplyr::sql("SELECT * FROM TMP_COMBINED_ADDRESSES")
-)
-
-# Tokenise the single line addresses and add token type ("D" = digit,
-# "C" = character)
-addressbase_plus_db <- addressbase_plus_db %>%
-  nhsbsaR::oracle_unnest_tokens(col = "SINGLE_LINE_ADDRESS", drop = FALSE) %>%
-  dplyr::mutate(TOKEN_TYPE = ifelse(REGEXP_LIKE(TOKEN, "[0-9]"), "D", "C"))
-
-# Compute query and save in temporary remote table
-addressbase_plus_db %>%
-  nhsbsaR::oracle_create_table(table_name = "TMP_ADDRESSBASE_PLUS")
-
-# Pull the temporary remote table
-addressbase_plus_db <- dplyr::tbl(
-  src = con,
-  from = dbplyr::sql("SELECT * FROM TMP_ADDRESSBASE_PLUS")
-)
-
-# Where DPA and GEO have different single line addresses for the same UPRN, get
-# a superset of all tokens
-combined_addresses_db <- combined_addresses_db %>%
-  dplyr::inner_join(
-    y = addressbase_plus_db %>% 
-      dplyr::select(UPRN, TOKEN, TOKEN_TYPE)
-  ) %>%
-  dplyr::distinct()
-
-# Indicate where each token has originated
-combined_addresses_db <- combined_addresses_db%>%
-  dplyr::left_join(
-    y = addressbase_plus_db %>%
-      dplyr::filter(ADDRESS_TYPE == "DPA") %>%
-      dplyr::mutate(DPA_TOKEN = 1) %>%
-      dplyr::select(-ADDRESS_TYPE, -SINGLE_LINE_ADDRESS, -TOKEN_NUMBER)
-  ) %>%
-  dplyr::left_join(
-    y = addressbase_plus_db %>%
-      dplyr::filter(ADDRESS_TYPE == "GEO") %>%
-      dplyr::mutate(GEO_TOKEN = 1) %>%
-      dplyr::select(-ADDRESS_TYPE, -SINGLE_LINE_ADDRESS, -TOKEN_NUMBER)
-  )
-
-# Filter combined addresses where the one DPA / GEO address is a subset of the 
-# other
-combined_addresses_db <- combined_addresses_db %>%
-  dplyr::inner_join(
-    y = combined_addresses_db %>%
-      dplyr::group_by(UPRN) %>%
-      dplyr::summarise(
-        MISSING_DPA_TOKENS = sum(ifelse(is.na(DPA_TOKEN), 1, 0)),
-        MISSING_GEO_TOKENS = sum(ifelse(is.na(GEO_TOKEN), 1, 0))
-      ) %>%
-      # If one address is missing 0 tokens then we don't need a combined address
-      dplyr::filter(MISSING_DPA_TOKENS > 0 & MISSING_GEO_TOKENS > 0) %>%
-      dplyr::select(UPRN)
-  ) %>%
-  dplyr::select(-DPA_TOKEN, -GEO_TOKEN)
-
-# Add the combined token rows to our data
-addressbase_plus_db <- addressbase_plus_db %>%
-  dplyr::union_all(
-    y = combined_addresses_db %>%
-      dplyr::mutate(ADDRESS_TYPE = "COMBINED")
-  )
-
-# Pull non unique postcode and single line address combinations
-non_unique_postcode_address_combinations_db <- addressbase_plus_db %>%
-  dplyr::distinct(UPRN, POSTCODE, SINGLE_LINE_ADDRESS) %>%
-  dplyr::count(POSTCODE, SINGLE_LINE_ADDRESS) %>% 
-  dplyr::filter(n > 1) %>%
-  dplyr::select(-n)
-
-# Add more of the address information
-non_unique_postcode_address_combinations_db <- addressbase_plus_db %>%
-  dplyr::select(-UPRN, TOKEN_NUMBER, -TOKEN, -TOKEN_NUMBER, -TOKEN_TYPE) %>%
-  dplyr::distinct() %>%
-  dplyr::inner_join(y = non_unique_postcode_address_combinations_db)
-
-# Update the care home flag on the non unique postcode and single line address
-# combinations. See if they have differing care home flags, if so allocate them 
-# all NA
-non_unique_postcode_address_combinations_care_home_flag_db <- 
-  non_unique_postcode_address_combinations_db %>%
-  dplyr::group_by(POSTCODE, SINGLE_LINE_ADDRESS) %>%
-  dplyr::summarise(
-    CH_FLAG_0 = sum(ifelse(CH_FLAG == 0, 1, 0)),
-    CH_FLAG_1 = sum(ifelse(CH_FLAG == 1, 1, 0)),
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(
-    CH_FLAG = dplyr::case_when(
-      CH_FLAG_0 > 0 & CH_FLAG_1 == 0 ~ 0,
-      CH_FLAG_1 > 0 & CH_FLAG_0 == 0 ~ 1,
-      TRUE ~ NA_integer_
-    )
-  ) %>%
-  dplyr::select(CH_FLAG, POSTCODE, SINGLE_LINE_ADDRESS)
-
-# Update the address type on the non unique postcode and single line address
-# combinations. See if they have differing address types, if so allocate them 
-# one based on priority: BOTH > DPA or GEO > NA.
-# NOTE: There are no COMBINED address types here
-non_unique_postcode_address_combinations_address_type_db <- 
-  non_unique_postcode_address_combinations_db %>%
-  dplyr::group_by(POSTCODE, SINGLE_LINE_ADDRESS) %>%
-  dplyr::summarise(
-    DPA = sum(ifelse(ADDRESS_TYPE == "DPA", 1, 0)),
-    GEO = sum(ifelse(ADDRESS_TYPE == "GEO", 1, 0)),
-    BOTH = sum(ifelse(ADDRESS_TYPE == "BOTH", 1, 0))
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(
-    ADDRESS_TYPE = dplyr::case_when(
-      BOTH > 0 ~ "BOTH",
-      BOTH == 0 & DPA > 0 & GEO == 0 ~ "DPA",
-      BOTH == 0 & GEO > 0 & DPA == 0 ~ "GEO",
-      TRUE ~ NA_character_
-    )
-  ) %>%
-  dplyr::select(ADDRESS_TYPE, POSTCODE, SINGLE_LINE_ADDRESS)
-  
-# Add the updated care home flag and address type for the non unique postcode
-# and single line address combinations
-non_unique_postcode_address_combinations_db <- 
-  non_unique_postcode_address_combinations_care_home_flag_db %>%
-  dplyr::inner_join(
-    y = non_unique_postcode_address_combinations_address_type_db
-  ) %>%
-  # Mock NA UPRN as we can't ever choose between 2
-  dplyr::mutate(UPRN = NA_integer_) %>%
-  dplyr::select(UPRN, CH_FLAG, ADDRESS_TYPE, POSTCODE, SINGLE_LINE_ADDRESS)
-
-# Add the token information 
-non_unique_postcode_address_combinations_db <- 
-  non_unique_postcode_address_combinations_db %>%
-  dplyr::inner_join(
-    y = addressbase_plus_db %>%
-    dplyr::distinct(SINGLE_LINE_ADDRESS, TOKEN_NUMBER, TOKEN, TOKEN_TYPE)
-  )
-
-# Update the duplicate address rows in the AddressBase Plus care home table
-addressbase_plus_db  <- addressbase_plus_db  %>%
-  dplyr::anti_join(
-    y = non_unique_postcode_address_combinations_db %>%
-      dplyr::distinct(POSTCODE, SINGLE_LINE_ADDRESS)
-  ) %>%
-  dplyr::union_all(y = non_unique_postcode_address_combinations_db)
+    cols = -c(UPRN, CLASS, CH_FLAG, AB_POSTCODE),
+    names_to = "ADDRESS_TYPE",
+    values_to = "AB_ADDRESS"
+  ) %>% 
+  filter(!is.na(AB_ADDRESS)) %>% 
+  select(UPRN, CLASS, CH_FLAG, AB_POSTCODE, AB_ADDRESS) %>% 
+  distinct() %>%
+  group_by(AB_POSTCODE, AB_ADDRESS) %>% 
+  mutate(UPRN_COUNT = dense_rank(UPRN)) %>% 
+  ungroup() %>% 
+  filter(UPRN_COUNT == 1) %>% 
+  select(-UPRN_COUNT) %>% 
+  arrange(UPRN) %>% 
+  mutate(UPRN_ID = row_number())
 
 # Write the table back to the DB
 addressbase_plus_db %>%
   nhsbsaR::oracle_create_table(table_name = "ADDRESSBASE_PLUS_CARE_HOME")
 
-# Drop the temporary tables
-DBI::dbRemoveTable(conn = con, name = "TMP_COMBINED_ADDRESSES")
-DBI::dbRemoveTable(conn = con, name = "TMP_ADDRESSBASE_PLUS")
+# Part Two: AddressBase plus base table tokens ---------------------------------
+
+# Create a lazy table OF the above ADDRESSBASE_PLUS_CARE_HOME table
+addressbase_plus_tk <- con %>% 
+  tbl(from = in_schema("ADNSH", "ADDRESSBASE_PLUS_CARE_HOME"))
+
+# Tokenise and generate UPRN-level count of int and char tokens
+addressbase_plus_tk <- addressbase_plus_tk %>% 
+  nhsbsaR::oracle_unnest_tokens(col = "AB_ADDRESS") %>% 
+  mutate(
+    INT_FLAG = ifelse(REGEXP_LIKE(TOKEN, '[0-9]'), 1, 0),
+    AB_CHAR_COUNT = ifelse(REGEXP_LIKE(TOKEN, '[0-9]'), 0, 1),
+    AB_TOTAL = 1
+  ) %>% 
+  group_by(UPRN_ID) %>% 
+  mutate(
+    AB_INT_COUNT = sum(INT_FLAG),
+    AB_CHAR_COUNT = sum(AB_CHAR_COUNT),
+    AB_TOTAL = sum(AB_TOTAL)
+  ) %>% 
+  ungroup() %>% 
+  select(
+    UPRN,
+    UPRN_ID,
+    AB_POSTCODE,
+    AB_ADDRESS = TOKEN,
+    CH_FLAG,
+    AB_INT_COUNT,
+    AB_CHAR_COUNT,
+    AB_TOTAL,
+    INT_FLAG
+    )
+
+# Write the table back to the DB
+addressbase_plus_tk %>%
+  nhsbsaR::oracle_create_table(table_name = "ADDRESSBASE_PLUS_CARE_HOME_TOKENS")
 
 # Disconnect from database
 DBI::dbDisconnect(con)
+
+#-------------------------------------------------------------------------------
