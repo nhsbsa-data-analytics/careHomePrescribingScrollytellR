cbf235c9d947aa1f14b9411436db0e09c80fabd6
kygoffe <kayoung.goffe1@nhs.net>
2021-11-24 16:51:32 +0000

    add geography selection

---
+++ R/mod_radio.R
@@ -0,0 +1,43 @@
+#' radio UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_radio_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    shinyWidgets::prettyRadioButtons(
+      inputId = ns("input_view"),
+      label = h4("Select geography:"),
+      choices = c("STP", "Local Authority"),
+      shape = "curve",
+      selected = "STP",
+      inline = TRUE
+    )
+  )
+}
+
+#' radio Server Functions
+#'
+#' @noRd
+mod_radio_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    return(
+      selected_geo <- reactive({
+        input$input_view
+      })
+    )
+  })
+}
+
+## To be copied in the UI
+# mod_radio_ui("radio_ui_1")
+
+## To be copied in the server
+# mod_radio_server("radio_ui_1")
