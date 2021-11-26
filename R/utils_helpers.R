03581acab37fb2b75dd9880953b00d78f293214d
adamivisonnhs <adam.ivison@nhs.net>
2021-11-26 11:42:34 +0000

    Add care home flag function

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -35,3 +35,93 @@ theme_nhsbsa <- function(hc, palette = NA, stack = "normal") {
     highcharter::hc_yAxis(title = list(text = "")) %>%
     highcharter::hc_credits(enabled = TRUE)
 }
+
+
+#' Add a care home flag
+#'
+#' Add a care home flag to the data
+#'
+#' @param df 
+#' @param address_col
+#' @param use_addressbase_plus_class_col
+#'
+#' @return
+#' @export
+calc_care_home_flag <- function(
+  df, 
+  address_col, 
+  use_addressbase_plus_class_col=FALSE
+) {
+  
+  df <- df %>%
+  
+    # Regex check for care home / non care home words (as the class isn't always
+    # perfect)
+    dplyr::mutate(
+      # If it has this in the name then it is probably a care home
+      # CARE-HOME / CARE HOME / NURSING-HOME / NURSING-HOME / etc
+      CH_WORD = ifelse(
+        test = (
+          {{ address_col }} %LIKE% "%CARE HOME%" |
+          {{ address_col }} %LIKE% "%CARE-HOME%" |
+          {{ address_col }} %LIKE% "%NURSING HOME%" |
+          {{ address_col }} %LIKE% "%NURSING-HOME%" |
+          {{ address_col }} %LIKE% "%RESIDENTIAL HOME%" |
+          {{ address_col }} %LIKE% "%RESIDENTIAL-HOME%" |
+          {{ address_col }} %LIKE% "%REST HOME%" |
+          {{ address_col }} %LIKE% "%REST-HOME%"
+        ),
+        yes = 1,
+        no = 0
+      ),
+      # If it has this in then it is definitely not a care home
+      NON_CH_WORD = ifelse(
+        test = (
+          {{ address_col }} %LIKE% "%ABOVE%" |
+          {{ address_col }} %LIKE% "%CARAVAN%" |
+          {{ address_col }} %LIKE% "%CHILDREN%" |
+          {{ address_col }} %LIKE% "%CONVENT%" |
+          {{ address_col }} %LIKE% "%HOLIDAY%" |
+          {{ address_col }} %LIKE% "%MARINA%" |
+          {{ address_col }} %LIKE% "%MOBILE%" |
+          {{ address_col }} %LIKE% "%MONASTERY%" |
+          {{ address_col }} %LIKE% "%NO FIXED ABODE%" |
+          {{ address_col }} %LIKE% "%RESORT%" |
+          {{ address_col }} %LIKE% "%RECOVERY%"
+        ),
+        yes = 1,
+        no = 0
+      )
+    )
+  
+  # Calculate the care home flag
+  if (use_addressbase_plus_class_col) {
+    # If we have the AddressBase plus class then we can use that
+    df <- df %>%
+      dplyr::mutate(
+        CH_FLAG = ifelse(
+          test = 
+            (CLASS == "RI01" | CH_WORD == 1) & (NON_CH_WORD == 0),
+          yes = 1,
+          no = 0
+        )
+      )
+    
+  } else {
+    # Otherwise just use the single line address field
+    df <- df %>%
+      dplyr::mutate(
+        CH_FLAG = ifelse(
+          test = CH_WORD == 1 & NON_CH_WORD == 0, 
+          yes = 1, 
+          no = 0
+        )
+      )
+    
+  }
+  
+  # Drop the calculated columns
+  df %>%
+    dplyr::select(-c(CH_WORD, NON_CH_WORD))
+    
+}
