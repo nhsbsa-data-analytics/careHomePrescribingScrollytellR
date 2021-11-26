4ee0a4ec8f6817ee331e1a3f73df39f406d7d682
kygoffe <kayoung.goffe1@nhs.net>
2021-11-26 09:43:29 +0000

    lint fix

--- R/mod_05_common_drugs.R
+++ R/mod_05_common_drugs.R
@@ -4,28 +4,25 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
-mod_05_common_drugs_ui <- function(id){
+#' @importFrom shiny NS tagList
+mod_05_common_drugs_ui <- function(id) {
   ns <- NS(id)
-  tagList(
- 
-  )
+  tagList()
 }
-    
+
 #' 05_common_drugs Server Functions
 #'
-#' @noRd 
-mod_05_common_drugs_server <- function(id){
-  moduleServer( id, function(input, output, session){
+#' @noRd
+mod_05_common_drugs_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
- 
   })
 }
-    
+
 ## To be copied in the UI
 # mod_05_common_drugs_ui("05_common_drugs_ui_1")
-    
+
 ## To be copied in the server
 # mod_05_common_drugs_server("05_common_drugs_ui_1")
