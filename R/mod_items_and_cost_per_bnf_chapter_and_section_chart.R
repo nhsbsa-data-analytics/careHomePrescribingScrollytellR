2d8c6966d2337efd2cd1009e983bf58672458736
adamivisonnhs <adam.ivison@nhs.net>
2021-11-29 14:14:32 +0000

    Add radio input for choice

--- R/mod_items_and_cost_per_bnf_chapter_and_section_chart.R
+++ R/mod_items_and_cost_per_bnf_chapter_and_section_chart.R
@@ -13,6 +13,14 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_ui <- function(id) {
     fluidRow(
       align = "center",
       style = "background-color: #FFFFFF;",
+      h6("Drugs prescribed to care home patients in England (2020/21)"),
+      radioButtons(
+        inputId = ns("metric"),
+        label = "Metric",
+        choices = c("Items", "Cost"),
+        inline = TRUE,
+        width = "100%"
+      ),
       highcharter::highchartOutput(
         outputId = ns("items_and_cost_per_bnf_chapter_and_section_chart"),
         height = "500px",
@@ -31,49 +39,56 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_server <- function(
   session
 ) {
   ns <- session$ns
-
-  # Hardcode metric for now
-  metric <- "COST"
+  
+  # Pull the input reactive value
+  input_metric <- reactive({ input$metric })
   
   # Limit based on metric
-  items_and_cost_per_bnf_chapter_and_section_df <- 
+  items_and_cost_per_bnf_chapter_and_section_df <-  reactive({
     careHomePrescribingScrollytellR::items_and_cost_per_bnf_chapter_and_section_df %>%
-    dplyr::filter(METRIC == metric)
+    dplyr::filter(METRIC == input_metric())
+  })
   
   # Define the colours (tint for the second level based on the %)
   # NOTE: Not happy with this but does the job for the moment
-  items_and_cost_per_bnf_chapter_and_section_df <-
-    items_and_cost_per_bnf_chapter_and_section_df %>%
-    dplyr::left_join(
-      y = items_and_cost_per_bnf_chapter_and_section_df %>%
-        dplyr::distinct(BNF_CHAPTER) %>%
-        cbind(COLOUR_LEVEL_1 = nhsbsaR::palette_nhsbsa())
-    ) %>%
-    dplyr::mutate(
-      PRP_LEVEL_2 = ifelse(is.na(PRP_LEVEL_2), 0, PRP_LEVEL_2),
-    )
-  items_and_cost_per_bnf_chapter_and_section_df$COLOUR_LEVEL_2 <-
-    Vectorize(tinter::lighten)(
-      items_and_cost_per_bnf_chapter_and_section_df$COLOUR_LEVEL_1,
-      items_and_cost_per_bnf_chapter_and_section_df$PRP_LEVEL_2
-    )
-  items_and_cost_per_bnf_chapter_and_section_df <- 
-    items_and_cost_per_bnf_chapter_and_section_df %>%
-    dplyr::mutate(
-      COLOUR_LEVEL_2 = as.character(COLOUR_LEVEL_2),
-      COLOUR_LEVEL_2 = ifelse(
-        test = COLOUR_LEVEL_2 == "character(0)", 
-        yes = "#FFFFFF", 
-        no = COLOUR_LEVEL_2
+  items_and_cost_per_bnf_chapter_and_section_df <- reactive({
+    
+    print("HERE")
+    
+    tmp_df <- items_and_cost_per_bnf_chapter_and_section_df() %>%
+      dplyr::left_join(
+        y = items_and_cost_per_bnf_chapter_and_section_df() %>%
+          dplyr::distinct(BNF_CHAPTER) %>%
+          cbind(COLOUR_LEVEL_1 = nhsbsaR::palette_nhsbsa())
+      ) %>%
+      dplyr::mutate(
+        PRP_LEVEL_2 = ifelse(is.na(PRP_LEVEL_2), 0, PRP_LEVEL_2),
       )
+    
+    tmp_df$COLOUR_LEVEL_2 <- Vectorize(tinter::lighten)(
+      tmp_df$COLOUR_LEVEL_1,
+      tmp_df$PRP_LEVEL_2
     )
+    
+    tmp_df %>%
+      dplyr::mutate(
+        COLOUR_LEVEL_2 = as.character(COLOUR_LEVEL_2),
+        COLOUR_LEVEL_2 = ifelse(
+          test = COLOUR_LEVEL_2 == "character(0)", 
+          yes = "#FFFFFF", 
+          no = COLOUR_LEVEL_2
+        )
+      )
+    
+  })
   
   # Format data for highcharter
-  plot_df <-
+  plot_df <- reactive({
+    
     dplyr::bind_rows(
       
       # BNF chapter
-      items_and_cost_per_bnf_chapter_and_section_df %>%
+      items_and_cost_per_bnf_chapter_and_section_df() %>%
         dplyr::distinct(BNF_CHAPTER, PRP_LEVEL_1, COLOUR_LEVEL_1) %>%
         dplyr::mutate(id = tolower(BNF_CHAPTER)) %>%
         dplyr::select(
@@ -84,7 +99,7 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_server <- function(
         ),
       
       # BNF section
-      items_and_cost_per_bnf_chapter_and_section_df %>%
+      items_and_cost_per_bnf_chapter_and_section_df() %>%
         dplyr::mutate(
           parent = tolower(BNF_CHAPTER),
           id = tolower(BNF_SECTION)
@@ -99,6 +114,8 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_server <- function(
       
     ) %>%
     highcharter::list_parse()
+    
+  })
   
   output$items_and_cost_per_bnf_chapter_and_section_chart <- 
     highcharter::renderHighchart({
@@ -106,7 +123,7 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_server <- function(
       highcharter::highchart() %>%
         highcharter::hc_chart(type = "treemap") %>%
         highcharter::hc_add_series(
-          data = plot_df,
+          data = plot_df(),
           allowDrillToNode = TRUE,
           levelIsConstant = FALSE,
           textOverflow = "clip",
@@ -130,9 +147,8 @@ mod_items_and_cost_per_bnf_chapter_and_section_chart_server <- function(
               dataLabels = list(enabled = FALSE)
             )
           )
-        ) %>%
-        highcharter::hc_title(text = "Drug group distribution for care home patients")
-  
+        )
+
   })
 }
 
