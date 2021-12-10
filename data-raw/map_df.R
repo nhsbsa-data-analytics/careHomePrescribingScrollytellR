ed2e684b5940122804f70bc1a7543902fcb80b5a
kygoffe <kayoung.goffe1@nhs.net>
2021-12-10 16:09:33 +0000

    styler lint

--- data-raw/map_df.R
+++ data-raw/map_df.R
@@ -2,7 +2,7 @@ library(dplyr)
 
 # All BUC boundary files @ CRS 27700
 map_df <- bind_rows(
-  
+
   # Region
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Regions_December_2020_EN_BUC_V2/FeatureServer/0/query?where=1%3D1&outFields=RGN20CD,RGN20NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "Region") %>%
@@ -12,7 +12,7 @@ map_df <- bind_rows(
       SUB_GEOGRAPHY_NAME = RGN20NM,
       GEOMETRY = geometry
     ),
-  
+
   # STP
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Sustainability_and_Transformation_Partnerships_April_2021_EN_BUC_v2/FeatureServer/0/query?where=1%3D1&outFields=STP21CD,STP21NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "STP") %>%
@@ -22,7 +22,7 @@ map_df <- bind_rows(
       SUB_GEOGRAPHY_NAME = STP21NM,
       GEOMETRY = geometry
     ),
-  
+
   # Local Authority
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LocalAuthorityDistrictsMay2021UKBUC/FeatureServer/0/query?where=1%3D1&outFields=LAD21CD,LAD21NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "Local Auhtority") %>%
@@ -32,7 +32,6 @@ map_df <- bind_rows(
       SUB_GEOGRAPHY_NAME = LAD21NM,
       GEOMETRY = geometry
     )
-  
 )
 
 # Add to data-raw/
