5d080e5d13dce3b8dfbb4fc40ac8ff0cd9137baf
adamivisonnhs <adam.ivison@nhs.net>
2021-12-10 13:48:29 +0000

    England only Local Authorities

--- data-raw/map_df.R
+++ data-raw/map_df.R
@@ -6,7 +6,7 @@ map_df <- bind_rows(
   # Region
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Regions_December_2020_EN_BUC_V2/FeatureServer/0/query?where=1%3D1&outFields=RGN20CD,RGN20NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "Region") %>%
-    dplyr::select(
+    select(
       GEOGRAPHY,
       SUB_GEOGRAPHY_CODE = RGN20CD,
       SUB_GEOGRAPHY_NAME = RGN20NM,
@@ -16,7 +16,7 @@ map_df <- bind_rows(
   # STP
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Sustainability_and_Transformation_Partnerships_April_2021_EN_BUC_v2/FeatureServer/0/query?where=1%3D1&outFields=STP21CD,STP21NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "STP") %>%
-    dplyr::select(
+    select(
       GEOGRAPHY,
       SUB_GEOGRAPHY_CODE = STP21CD,
       SUB_GEOGRAPHY_NAME = STP21NM,
@@ -26,12 +26,13 @@ map_df <- bind_rows(
   # Local Authority
   sf::read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LocalAuthorityDistrictsMay2021UKBUC/FeatureServer/0/query?where=1%3D1&outFields=LAD21CD,LAD21NM&outSR=27700&f=json") %>%
     mutate(GEOGRAPHY = "Local Authority") %>%
-    dplyr::select(
+    select(
       GEOGRAPHY,
       SUB_GEOGRAPHY_CODE = LAD21CD,
       SUB_GEOGRAPHY_NAME = LAD21NM,
       GEOMETRY = geometry
-    )
+    ) %>%
+    filter(substr(SUB_GEOGRAPHY_CODE, 1, 1) == "E")
   
 )
 
