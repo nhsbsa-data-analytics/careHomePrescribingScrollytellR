d442346ad450f642689308c3f1bfbe6119f63564
adamivisonnhs <adam.ivison@nhs.net>
2021-12-16 15:08:23 +0000

    Move definitions to the end of the server

--- R/app_server.R
+++ R/app_server.R
@@ -13,9 +13,9 @@ app_server <- function(input, output, session) {
   mod_03_overall_summary_server("03_overall_summary_ui_1")
   mod_04_estimated_care_home_patients_server("04_estimated_care_home_patients_ui_1")
   mod_05_items_and_cost_per_bnf_chapter_and_section_chart_server("05_items_and_cost_per_bnf_chapter_and_section_chart_ui_1")
-  mod_definitions_server("definitions_ui_1")
   mod_08_footer_server("08_footer_ui_1")
-
+  mod_definitions_server("definitions_ui_1")
+  
   output$scrolly <- scrollytell::renderScrollytell({
     scrollytell::scrollytell()
   })
